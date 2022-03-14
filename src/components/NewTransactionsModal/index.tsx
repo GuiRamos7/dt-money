b156df2481b74177be7a49db09e630b80a32fa62
Guilherme Ramos <guiramos@DESKTOP-LO9LDT6.localdomain>
2022-03-14 05:43:27 -0300

    creating button of type trasaction on modal to create a new transaction

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,5 +1,7 @@
 import * as S from './styles';
 import closeImg from 'assets/close.svg';
+import incomeImg from 'assets/income.svg';
+import outcomeImg from 'assets/outcome.svg';
 
 interface INewTransactionsModal {
   isOpen: boolean;
@@ -17,13 +19,27 @@ const NewTransactionsModal = ({
       onRequestClose={onRequestClose}
       isOpen={isOpen}
     >
-      <button type='button'>
+      <button type='button' className='react-modal-close-icon'>
         <img src={closeImg} alt='Close modal' onClick={onRequestClose} />
       </button>
       <S.Container>
         <h2>Register a transaction</h2>
         <input placeholder='Name' />
         <input placeholder='Value' type='number' />
+        <S.TransactionTypeContainer>
+          <button type='button'>
+            <img src={incomeImg} alt='Received icon' onClick={onRequestClose} />
+            <span>Received</span>
+          </button>
+          <button type='button'>
+            <img
+              src={outcomeImg}
+              alt='Withdraw icon'
+              onClick={onRequestClose}
+            />
+            <span>Withdraw</span>
+          </button>
+        </S.TransactionTypeContainer>
         <input placeholder='category' />
         <button type='submit'>Register</button>
       </S.Container>
