f9e0f786e1370395b2e4de671af56e6b34003445
Guilherme Ramos <guiramos@DESKTOP-LO9LDT6.localdomain>
2022-03-14 03:05:14 -0300

    config the miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createServer } from 'miragejs';
+
 import App from './App';
 import { GlobalStyle } from './styles/global';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Playstation Store',
+          amount: 400,
+          type: 'withdraw',
+          category: 'Games',
+          createdAt: new Date(),
+        },
+        {
+          id: 2,
+          title: 'Salary',
+          amount: 5000,
+          type: 'deposit',
+          category: 'Salary',
+          createdAt: new Date(),
+        },
+        {
+          id: 3,
+          title: 'Sushi',
+          amount: 44.55,
+          type: 'withdraw',
+          category: 'Games',
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
